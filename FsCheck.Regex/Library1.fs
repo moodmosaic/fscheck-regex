6b0f7a355c11c856e75f2cb4c1e850b5db2c1345
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2015-10-07 11:35:04 +0300

    Run 'paket install', to create paket.lock and reference Fare, and FsCheck.Xunit, in FsCheck.Regex project.

--- FsCheck.Regex/Library1.fs
+++ FsCheck.Regex/Library1.fs
@@ -1,4 +1,21 @@
-﻿namespace FsCheck.Regex
+﻿module FsCheck.Regex.Tests
 
-type Class1() = 
-    member this.X = "F#"
+open Fare
+open FsCheck
+open FsCheck.Xunit
+open System.Text.RegularExpressions
+
+let matching pattern =
+    Gen.sized (fun size ->
+        let xeger = Xeger pattern
+        let count = if size < 1 then 1 else size
+        [ for i in 1..count -> xeger.Generate() ]
+        |> Gen.elements
+        |> Gen.resize count)
+
+[<Property>]
+let ``Turns a regular expression into a generator of strings matching that regex``() =
+    let pattern = "^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$"
+    let matches = Arb.fromGen (matching pattern)
+
+    Prop.forAll matches (fun s -> Regex.IsMatch(s, pattern))
--- FsCheck.Regex/Script.fsx
+++ FsCheck.Regex/Script.fsx
@@ -1,8 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
 
-#load "Library1.fs"
-open FsCheck.Regex
+#r "../packages/FsCheck/lib/net45/FsCheck.dll"
 
-// Define your library scripting code here
 
+open FsCheck
+open System
+
+let matching pattern =
+    Gen.sized (fun size ->
+        let xeger = Xeger pattern
+        let count = if size < 1 then 1 else size
+        [ for i in 1..count -> xeger.Generate() ]
+        |> Gen.elements
+        |> Gen.resize count)
+
+let generate = Gen.sample 1000 9
+
+let a = generate (matching "[0-9a-z]")
+let b = generate (matching "[0-9a-z]")
+let c = generate (matching "[0-9a-z]")
+
+let d = generate (matching "\d{1,3}\.\d{1,3}\.\d{1,3}")
+let e = generate (matching "\d{1,3}\.\d{1,3}\.\d{1,3}")
+let f = generate (matching "\d{1,3}\.\d{1,3}\.\d{1,3}")
+
+let g = generate (matching "^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$")
+let h = generate (matching "^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$")
+let i = generate (matching "^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$")
+
+let j = generate (matching "^[a-zA-Z''-'\s]{1,40}$")
+let k = generate (matching "^[a-zA-Z''-'\s]{1,40}$")
+let l = generate (matching "^[a-zA-Z''-'\s]{1,40}$")
